f1f1c8d2caa090a439c4624fe08385b0af36bb90
Jonathan Ben-Hamou <jbenhamou@icoegroup.com>
2016-05-11 18:58:27 -0700

    Adding basic authorization (not working)

--- m1-lesson3/src/main/java/com/baeldung/lss/spring/LssSecurityConfig.java
+++ m1-lesson3/src/main/java/com/baeldung/lss/spring/LssSecurityConfig.java
@@ -1,13 +1,18 @@
 package com.baeldung.lss.spring;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
 public class LssSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private final Log logger = LogFactory.getLog(LssSecurityConfig.class);
+    
     public LssSecurityConfig() {
         super();
     }
@@ -22,4 +27,15 @@ public class LssSecurityConfig extends WebSecurityConfigurerAdapter {
             roles("USER");
     } // @formatter:on
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception { // formatter:off
+        logger.debug("Overriding configure(HttpSecurity).");
+
+        http
+                .authorizeRequests()
+                        .anyRequest().authenticated()
+                        .antMatchers("/delete/**").hasRole("ADMIN")
+                        .and()
+                .formLogin().and()
+                .httpBasic();    } // formatter:on
 }

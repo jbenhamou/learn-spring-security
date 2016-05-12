bd002c5a020f041a465ad63e1bcefdbbaef9b5d5
Jonathan Ben-Hamou <jbenhamou@icoegroup.com>
2016-05-11 19:08:38 -0700

    Deleting a user requires ROLE_ADMIN authority
    It is vital to place authorization expression before authentication
    
    The video tutorial is showing the following code:
    .anyRequest().authenticated()
    .antMatchers("/delete/**").hasRole("ADMIN")
    
    Which results in in the delete operation being permitted.
    
    The correct expression is:
    
    .antMatchers("/delete/**").hasRole("ADMIN")
    .anyRequest().authenticated()

--- m1-lesson3/src/main/java/com/baeldung/lss/spring/LssSecurityConfig.java
+++ m1-lesson3/src/main/java/com/baeldung/lss/spring/LssSecurityConfig.java
@@ -33,8 +33,8 @@ public class LssSecurityConfig extends WebSecurityConfigurerAdapter {
 
         http
                 .authorizeRequests()
-                        .anyRequest().authenticated()
                         .antMatchers("/delete/**").hasRole("ADMIN")
+                        .anyRequest().authenticated()
                         .and()
                 .formLogin().and()
                 .httpBasic();    } // formatter:on
